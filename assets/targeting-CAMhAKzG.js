5eadedcf077003f31045da75b5dbed48a2d299ba
leolnchaves <leolnchaves@users.noreply.github.com>
2026-09-21 16:53:27 +0000

    deploy: 15d34b5b21ff4f6a588c545847d47b269cf214e5

---
+++ assets/targeting-CAMhAKzG.js
@@ -0,0 +1,1 @@
+import{t as e}from"./jsx-runtime-B-hcVAMW.js";import{n as t}from"./application-session-AXBKaUtw.js";import{t as n}from"./product-route-page-Kx8-okRn.js";var r=e(),i=()=>(0,r.jsx)(t,{children:(0,r.jsx)(n,{productId:`targeting`})});export{i as component};
