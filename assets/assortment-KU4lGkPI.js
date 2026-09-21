0e9213ecf8e73dba8c047c4ad6c47de7ca0f83ef
leolnchaves <leolnchaves@users.noreply.github.com>
2026-09-21 16:35:59 +0000

    deploy: 6884385b113d2be0504a43e8475656a4c8873a4e

--- assets/assortment-KU4lGkPI.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./jsx-runtime-B-hcVAMW.js";import{n as t}from"./application-session-CtryBPJs.js";import{t as n}from"./product-route-page-BZZejeWI.js";var r=e(),i=()=>(0,r.jsx)(t,{children:(0,r.jsx)(n,{productId:`assortment`})});export{i as component};
---
+++ assets/targeting-CrjguRD6.js
@@ -0,0 +1,1 @@
+import{t as e}from"./jsx-runtime-B-hcVAMW.js";import{n as t}from"./application-session-DXXxok41.js";import{t as n}from"./product-route-page-BSLQZNCz.js";var r=e(),i=()=>(0,r.jsx)(t,{children:(0,r.jsx)(n,{productId:`targeting`})});export{i as component};
